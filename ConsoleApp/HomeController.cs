53d617e9820f345f3c43a643f9c1e200a130c345
DeepakThakurPatna <108866773+DeepakThakurPatna@users.noreply.github.com>
2023-03-15 21:55:06 +0530

    Add project files.

---
+++ ConsoleApp/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Deepak Thakur";
+        }
+    }
+}
